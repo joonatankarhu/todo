6198388ac18fa223076e4fbab84b7fb0838a5988
Joona <joonatan.karhu@outlook.com>
2022-07-19 19:06:24 +0300

    Quick fix

--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -5,16 +5,13 @@ import CreateTaskModule from '../Components/CreateTaskModule';
  
 const Home = () => {
     const [show, setShow] = useState(false);
-    const [task, setTask] = useState(['abc', 'abc']);
-
-
 
     return (
         <div className='p-5'>
             <h2 className='text-3xl mb-5'>Weekly Tasks</h2>
             <div onClick={() => setShow(true)} className='bg-green-500 inline py-2 px-5 rounded text-white font-medium hover:bg-green-700 hover:cursor-pointer'>New Task</div>
             <div>
-                <ul>
+                {/* <ul>
                     {
                         task.map((task) => {
                             <li key={task}>
@@ -22,7 +19,7 @@ const Home = () => {
                             </li>
                         })
                     }
-                </ul>
+                </ul> */}
             </div>
             <CreateTaskModule hideModule={() => setShow(false)} show={show} />
         </div>
